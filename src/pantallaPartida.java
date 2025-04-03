a0153c7ad23e4ec1e2e0b362430f0e103d70f042
iannunez-max <iannumi24@alumnes.ilerna.com>
2025-04-03 16:42:17 +0200

    pantallaPartida empezado

---
+++ src/pantallaPartida.java
@@ -0,0 +1,24 @@
+public class pantallaPartida {
+    private Tablero tablero;
+
+    void botonTirarDado(){
+
+    }
+
+    void botonUsarObjeto(){
+
+    }
+
+    void botonFinalizarTurno(){
+
+    }
+    void iniciarPartida(){
+
+    }
+    void cargarPartida(){
+
+    }
+    void guardarPartida(Tablero t){
+
+    }
+}
