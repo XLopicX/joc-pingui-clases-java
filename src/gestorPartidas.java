8a54c1b8cf080995bc81afae12a8a10067c150b6
aleix gonzalez <aleixgoungra@alumnes.ilerna.com>
2025-04-03 17:11:32 +0200

    clase empezada

---
+++ src/gestorPartidas.java
@@ -0,0 +1,38 @@
+public class gestorPartidas {
+
+    private String urlBBDD;
+    private String username;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUrlBBDD() {
+        return urlBBDD;
+    }
+
+    public void setUrlBBDD(String urlBBDD) {
+        this.urlBBDD = urlBBDD;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    void guardarPartida(Tablero t) {
+
+    }
+
+    void Tablero cargarPartida() {
+
+    }
+}
